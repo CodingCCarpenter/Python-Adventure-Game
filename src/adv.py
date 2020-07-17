ef22d4edd48bcaf78d071b0b3b0288a2b0615141
CodingCCarpenter <Christineassists@aol.com>
2020-07-16 21:08:01 -0500

    declare all rooms and link together

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,31 @@
 from room import Room
+from player import Player 
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north, east, and west."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'chamber': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+
+    'common_room': Room("Common Room", """Welcome to the Ravenclaw Common Room!
+This room bends from west to north. 
+Don't forget to say hello to Luna 
+on your way to get the gold!"""),
+
+    'passage': Room('Wide Passage', """A wide passage is laid out before you. 
+There is a mysterious mist and you cannot 
+see the end of it. You think you seem a 
+dim light in the east.""")
 }
 
 
@@ -27,11 +34,19 @@ earlier adventurers. The only exit is to the south."""),
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
+# room['foyer'].e_to = room['narrow']
 room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
+room['common_room'].n_to = room['chamber']
+room['chamber'].s_to = room['common_room']
+room['foyer'].e_to = room['common_room']
+room['common_room'].w.to = room['foyer']
+room['passage'].e_to = room['foyer']
+room['foyer'].w_to = room['passage']
+
+
 
 #
 # Main
